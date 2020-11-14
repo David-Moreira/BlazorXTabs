418cc2c129adf7dfb468be2d24be2af20dac0ad2
David-Moreira <david_nogueira_moreira@hotmail.com>
2020-11-14 23:49:09 +0000

    Added RenderMode Added TabAdded and ActiveTabChanged Events

---
+++ src/XTabs/Configuration/RenderMode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorXTabs.Configuration
+{
+    /// <summary>
+    /// Partial: Will only render the active tab
+    /// <para>Full: Will always fully render tabs, but hide them</para>
+    /// Steps: Will render a Wizard like component
+    /// </summary>
+    public enum RenderMode
+    {
+        Partial,
+        Full,
+        Steps
+    }
+}
